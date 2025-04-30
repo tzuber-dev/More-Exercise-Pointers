5ed343705108e8dce82bd9afa29d89425b2d8dce
tzuber-dev <zt50481@mail.harpercollege.edu>
2025-04-30 13:41:38 -0500

    More Pointers Exercise

---
+++ NewPoiinterExercises.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+string str = "a string";
+
+
+
+//int solution1() {
+//	int A, B;
+//	cout << "Enter first number";
+//	cin >> A;
+//	cout << "Enter second number";
+//	cin >> B;
+//	int* ptrA = &A;
+//	int* ptrB = &B;
+//	cout << endl << A << endl << &A << endl << B << endl << &B << endl << ptrA << endl << &ptrA << endl << ptrB << endl << &ptrB;
+//}
+//int solution2 () {
+//	vector<int> vect = { 1,2,3,4,5,6,7,8,9,0 };
+//	int max = vect[0];
+//	int* pos = &max;
+//	for (int i = 0; i < vect.size(); i++) {
+//		{
+//			if (vect[i] > max) { max = vect[i];
+//			pos = &vect[i];
+//			}
+//		}
+//	}
+//	cout<< pos;
+//}
+//int soulution3() {
+//	int value;
+//	cout << "Enter value" << endl;
+//		cin>>value;
+//		int* George = &value;
+//		cout << *George;
+//}
+//int solution4() {
+//	string str = "a string";
+//	char* ptr= &str[0];
+//	cout << *ptr<< " "<< &ptr << " " << *(ptr+3)<<endl;
+//	ptr = ptr + 2;
+//	cout << ptr << " " << *(ptr + 2) << *(ptr + 5);
+//}
+int main() {
+char *take = str[0]
+
+}
